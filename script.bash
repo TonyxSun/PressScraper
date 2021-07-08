d1d727b2aea0a4bbf56cba26150f23ef2efe06f8
Tony Sun <sunwuyue@live.com>
2021-07-08 10:58:12 -0700

    update bash file

--- script.bash
+++ script.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python3 daily_digest_spider.py
-python3 fcc_spider.py
-python3 dailyBillsSenate.py
-python3 sbanking_spider.py
-python3 scommerce_spider.py
-python3 sforeign_spider.py
-python3 sfinance_spider.py
-python3 shlsga_spider.py
-python3 sjudiciary_spider.py
-python3 sia_spider.py
-python3 floor_spider.py
-python3 rollCallLists_Senate.py
+python3 ./Senate/daily_digest_spider.py
+python3 ./Industry/fcc_spider.py
+python3 ./Senate/dailyBillsSenate.py
+python3 ./Senate/sbanking_spider.py
+python3 ./Senate/scommerce_spider.py
+python3 ./Senate/sforeign_spider.py
+python3 ./Senate/sfinance_spider.py
+python3 ./Senate/shlsga_spider.py
+python3 ./Senate/sjudiciary_spider.py
+python3 ./Industry/sia_spider.py
+python3 ./Senate/floor_spider.py
+python3 ./Senate/rollCallLists_Senate.py

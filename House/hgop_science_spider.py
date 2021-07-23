75d8ccaf15454d58ac05b7be9239855a644d1eb0
verxia <76971655+verxia@users.noreply.github.com>
2021-07-23 13:48:30 -0700

    Republican house science spider

--- House/hgop_science_spider.py
+++ House/hgop_science_spider.py
@@ -47,20 +47,30 @@ class HouseGOPScienceSpider(scrapy.Spider):
         category = response.meta["category"]
         
         for item in response.css('[class^="views-row"]'):
-            
+
+            # Get all text
             all_content = item.css('[class="field-content"] *::text').getall()
-            date = get_date(all_content[1])
             
+            # For news, get date as an object
+            if category == "News":
+                date = all_content[1]
+                date_obj = datetime.strptime(date, "%b %d, %Y").date()
             
+            # For hearings and markups, get date as an object
+            elif category in ["Hearings", "Markups"]:
+                date = get_date(all_content[1])
+                date_obj = datetime.strptime(date, "%m/%d/%Y").date()
+            
+            # Continue and extract if date is valid
+            if check_date(date_obj):
                 
-            
-            yield {
-                'category': category,
-                'date': date,
-                'title': all_content[0],
-                'url': response.urljoin(item.css('[class="field-content"] a::attr(href)').get()),
-            }
-                
+                yield {
+                    'category': category,
+                    'date': date,
+                    'title': all_content[0],
+                    'url': response.urljoin(item.css('[class="field-content"] a::attr(href)').get()),
+                }
+                        
         
 
 

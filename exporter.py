f15bb301ed22fe0e9674141d10c7583614437268
Tony Sun <sunwuyue@live.com>
2021-07-26 00:34:47 -0700

    format

--- exporter.py
+++ exporter.py
@@ -7,7 +7,7 @@ list of not included items:
 
 Congress: All Bills
 Senate: Roll Call Votes
-Senate: Floor ACtivity (tentative)
+Senate: Floor ACtivity 
 
 '''
 date = datetime.today().strftime("%m.%d.%y")
@@ -37,7 +37,7 @@ html_text += fcc_html
 html_text += '<h2 id="us-congress">US Congress</h2>'
 
 #daily dIgest
-html_text += '<strong>Daily Digests</strong>: Date, URL, and text providing details of legislation introduced, reported, passed, and considered by the full House or Senate each legislative day;<sub> <br> <em><a href="https://www.congress.gov/bills-with-chamber-action/browse-by-date">https://www.congress.gov/bills-with-chamber-action/browse-by-date</a></em>'
+html_text += '<strong>Daily Digests</strong>: Date, URL, and text providing details of legislation introduced, reported, passed, and considered by the full House or Senate each legislative day;<sub> <br> <em><a href="https://www.congress.gov/bills-with-chamber-action/browse-by-date">https://www.congress.gov/bills-with-chamber-action/browse-by-date</a></em></sub>'
 
 digest = pd.read_csv(rf'Congress\output\digest_'+ date + '.csv')
 digest_html = digest.to_html()
@@ -45,7 +45,7 @@ html_text += digest_html
 
 #Daily Bills 
 #NOTE: TO BE REMOVED IF TOO LONG A LIST??
-html_text += '<strong>Daily Bill Texts</strong>: Date, PDF file, and text providing detailed information on legislation considered in <strong>Daily Digests</strong>;<sub>  <br><em><a href="https://www.congress.gov/bill-texts-received-today">https://www.congress.gov/bill-texts-received-today</a></em>'
+html_text += '<strong>Daily Bill Texts</strong>: Date, PDF file, and text providing detailed information on legislation considered in <strong>Daily Digests</strong>;<sub>  <br><em><a href="https://www.congress.gov/bill-texts-received-today">https://www.congress.gov/bill-texts-received-today</a></em></sub>'
 
 bills = pd.read_csv(rf'Congress\output\daily_bills_'+ date + '.csv')
 bills_html = bills.to_html()
@@ -55,52 +55,52 @@ html_text += bills_html
 html_text += '<h2 id="us-senate">US Senate</h2>'
 
 #floor
-html_text += '<strong>Floor Activity</strong>: Date, URL, and text providing details of senate floor proceedings;<sub>   <em><a href="https://floor.senate.gov/proceedings">https://floor.senate.gov/proceedings</a></em>'
+# html_text += '<strong>Floor Activity</strong>: Date, URL, and text providing details of senate floor proceedings;<sub>   <em><a href="https://floor.senate.gov/proceedings">https://floor.senate.gov/proceedings</a></em>'
 
-floor = pd.read_csv(rf'Senate\output\floor_'+ date + '.csv')
-floor_html = floor.to_html()
-html_text += floor_html
+# floor = pd.read_csv(rf'Senate\output\floor_'+ date + '.csv')
+# floor_html = floor.to_html()
+# html_text += floor_html
 
 #Senate committees
 html_text += '<h3 id="us-senate-committees">US Senate Committees</h3>'
 
 #Commerce
-html_text += '<strong>Commerce</strong>:  Date, URL. title, and summary of press releases, hearings, and markups from the US Senate Committee on Commerce, Science, and Transportation;<sub>  <em><a href="https://www.commerce.senate.gov/pressreleases">https://www.commerce.senate.gov/pressreleases</a></em>, <em><a href="https://www.commerce.senate.gov/hearings">https://www.commerce.senate.gov/hearings</a></em>, <em><a href="https://www.commerce.senate.gov/markups">https://www.commerce.senate.gov/markups</a></em>'
+html_text += '<strong>Commerce</strong>:  Date, URL. title, and summary of press releases, hearings, and markups from the US Senate Committee on Commerce, Science, and Transportation;<sub>  <em><a href="https://www.commerce.senate.gov/pressreleases">https://www.commerce.senate.gov/pressreleases</a></em>, <em><a href="https://www.commerce.senate.gov/hearings">https://www.commerce.senate.gov/hearings</a></em>, <em><a href="https://www.commerce.senate.gov/markups">https://www.commerce.senate.gov/markups</a></em></sub>'
 
 scomm = pd.read_csv(rf'Senate\output\scommerce_'+ date + '.csv')
 scomm_html = scomm.to_html()
 html_text += scomm_html
 
 #Foreign
-html_text += '<strong>Foreign</strong>:  Type of content (nomiations, treaties, legislation, hearing transcripts, business meeting transcripts, committee reports, other), date, URL (if given), and text for activities and reports from the US Senate Committee on Foreign Relations;<sub>  <em><a href="https://www.foreign.senate.gov/activities-and-reports">https://www.foreign.senate.gov/activities-and-reports</a></em>'
+html_text += '<strong>Foreign</strong>:  Type of content (nomiations, treaties, legislation, hearing transcripts, business meeting transcripts, committee reports, other), date, URL (if given), and text for activities and reports from the US Senate Committee on Foreign Relations;<sub>  <em><a href="https://www.foreign.senate.gov/activities-and-reports">https://www.foreign.senate.gov/activities-and-reports</a></em></sub>'
 
 sforeign_ = pd.read_csv(rf'Senate\output\sforeign_'+ date + '.csv')
 sforeign_html = sforeign_.to_html()
 html_text += sforeign_html
 
 #Banking
-html_text += '<strong>Banking</strong>: Date, URL, and title for press releases, hearings, and markups from the US Senate Committee on Banking, Housing, and Urban Affairs;<sub>  <em><a href="https://www.banking.senate.gov/newsroom/majority-press-releases">https://www.banking.senate.gov/newsroom/majority-press-releases</a></em>, <em><a href="https://www.banking.senate.gov/hearings">https://www.banking.senate.gov/hearings</a></em>, <em><a href="https://www.banking.senate.gov/markups">https://www.banking.senate.gov/markups</a></em>'
+html_text += '<strong>Banking</strong>: Date, URL, and title for press releases, hearings, and markups from the US Senate Committee on Banking, Housing, and Urban Affairs;<sub>  <em><a href="https://www.banking.senate.gov/newsroom/majority-press-releases">https://www.banking.senate.gov/newsroom/majority-press-releases</a></em>, <em><a href="https://www.banking.senate.gov/hearings">https://www.banking.senate.gov/hearings</a></em>, <em><a href="https://www.banking.senate.gov/markups">https://www.banking.senate.gov/markups</a></em></sub>'
 
 sbanking_ = pd.read_csv(rf'Senate\output\sbanking_'+ date + '.csv')
 sbanking_html = sbanking_.to_html()
 html_text += sbanking_html
 
 #Finance
-html_text += '<strong>Finance</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on Finance;<sub>  <em><a href="https://www.finance.senate.gov/chairmans-news">https://www.finance.senate.gov/chairmans-news</a></em>, <em><a href="https://www.finance.senate.gov/hearings">https://www.finance.senate.gov/hearings</a></em>'
+html_text += '<strong>Finance</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on Finance;<sub>  <em><a href="https://www.finance.senate.gov/chairmans-news">https://www.finance.senate.gov/chairmans-news</a></em>, <em><a href="https://www.finance.senate.gov/hearings">https://www.finance.senate.gov/hearings</a></em></sub>'
 
 sfinance = pd.read_csv(rf'Senate\output\sfinance'+ date + '.csv')
 sfinance_html = sfinance.to_html()
 html_text += sfinance_html
 
 #HSGAc
-html_text += '<strong>HLSGA</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on Homeland Security &amp; Government Affairs;<sub>  <em><a href="https://www.hsgac.senate.gov/media/majority-media">https://www.hsgac.senate.gov/media/majority-media</a></em>, <em><a href="https://www.hsgac.senate.gov/hearings">https://www.hsgac.senate.gov/hearings</a></em>'
+html_text += '<strong>HLSGA</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on Homeland Security &amp; Government Affairs;<sub>  <em><a href="https://www.hsgac.senate.gov/media/majority-media">https://www.hsgac.senate.gov/media/majority-media</a></em>, <em><a href="https://www.hsgac.senate.gov/hearings">https://www.hsgac.senate.gov/hearings</a></em></sub>'
 
 shsgac = pd.read_csv(rf'Senate\output\shsgac'+ date + '.csv')
 shsgac_html = shsgac.to_html()
 html_text += shsgac_html
 
 #judiciary
-html_text += '<strong>Judiciary</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on the Judiciary;<sub>  <em><a href="https://www.judiciary.senate.gov/press/majority">https://www.judiciary.senate.gov/press/majority</a></em>, <em><a href="https://www.judiciary.senate.gov/hearings">https://www.judiciary.senate.gov/hearings</a></em>'
+html_text += '<strong>Judiciary</strong>: Source of content (majority, minority), date, URL, and title for press releases and hearings from the US Senate Committee on the Judiciary;<sub>  <em><a href="https://www.judiciary.senate.gov/press/majority">https://www.judiciary.senate.gov/press/majority</a></em>, <em><a href="https://www.judiciary.senate.gov/hearings">https://www.judiciary.senate.gov/hearings</a></em></sub>'
 
 sjudiciary = pd.read_csv(rf'Senate\output\sjudiciary'+ date + '.csv')
 sjudiciary_html = sjudiciary.to_html()

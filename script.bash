577a6321a3c86c7d63fc024cb67ebe6a9e5c9a33
verxia <76971655+verxia@users.noreply.github.com>
2021-07-27 09:38:40 -0700

    Output for Jul 27

--- script.bash
+++ script.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ./Senate
+rm output/*
 python3 sbanking_spider.py
 python3 scommerce_spider.py
 python3 sforeign_spider.py
@@ -11,6 +12,7 @@ python3 senate_floor_spider.py
 python3 senate_roll_call.py
 
 cd ../House
+rm output/*
 python3 h_energy_spider.py
 python3 h_financial_spider.py
 python3 h_foreign_spider.py
@@ -23,10 +25,12 @@ python3 hgop_homeland_spider.py
 python3 hgop_science_spider.py
 
 cd ../Industry
+rm output/*
 python3 sia_spider.py
 python3 fcc_spider.py
 
 cd ../Congress
+rm output/*
 python3 daily_digest_spider.py
 python3 daily_bills.py
 python3 all_bills_spider.py

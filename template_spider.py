f2c7ed21882cf8da7a4e2fe1deffcd6fc17d4e70
verxia <76971655+verxia@users.noreply.github.com>
2021-08-16 20:16:03 -0400

    Update for Aug 16, 2021

---
+++ template_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+from datetime import datetime
+from scrapy import cmdline
+
+import sys
+sys.path.insert(1, '../.')
+from check_date import check_date
+
+
+
+class TemplateSpider(scrapy.Spider):
+    name = "template"
+    
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.google.ca', callback=self.parse)
+    
+    
+    def parse(self, response):
+        
+        for item in response.css():
+            pass
+
+
+# ALSO UPDATE ...
+# script.bash
+# 
+
+
+# Creates file with date and writes content to the file
+# os.system("touch fcc_$(date +%m.%d.%y).csv")
+date = datetime.today().strftime("%m.%d.%y")
+execute = "scrapy runspider template_spider.py -O output/template_" + date + ".csv"
+cmdline.execute(execute.split())
